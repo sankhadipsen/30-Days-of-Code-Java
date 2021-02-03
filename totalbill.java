fa43dace051458ee46be653abecd250348d953d4
Sankhadip Sen <sankhadip.sen@gmail.com>
2021-02-03 13:03:26 +0530

    Create totalbill.java
    Calculates the bill based on the food bill and adds tip and tax.

---
+++ totalbill.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+public class Solution2 {
+
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        double total;
+        total = Math.round(meal_cost+(meal_cost*(tip_percent/100))+(meal_cost*(tax_percent/100)));
+        System.out.println(total);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double meal_cost = scanner.nextDouble();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tip_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tax_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+}
